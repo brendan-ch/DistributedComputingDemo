405f465ecb8d5ce7d3ef9ceadbedd77694542b88
Brendan Chen <me@bchen.dev>
2025-04-12 19:33:42 -0700

    Initial Commit

---
+++ DistributedComputingDemo/DistributedComputingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistributedComputingDemoApp.swift
+//  DistributedComputingDemo
+//
+//  Created by Brendan Chen on 2025.04.12.
+//
+
+import SwiftUI
+
+@main
+struct DistributedComputingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
