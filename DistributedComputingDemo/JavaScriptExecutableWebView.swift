390733d7fc380e96b4bf1d0c14b5138a6593e446
Brendan Chen <me@bchen.dev>
2025-04-12 21:14:29 -0700

    add a completion handler to the web view

--- DistributedComputingDemo/JavaScriptExecutableWebView.swift
+++ DistributedComputingDemo/JavaScriptExecutableWebView.swift
@@ -10,6 +10,7 @@ import WebKit
 
 struct JavaScriptExecutableWebView: UIViewRepresentable {
     let javascriptString: String
+    let completionHandler: ((_ result: Any?, _ error: Any?) -> Void)?
     
     func makeUIView(context: Context) -> WKWebView {
         let config = WKWebViewConfiguration()
@@ -23,6 +24,14 @@ struct JavaScriptExecutableWebView: UIViewRepresentable {
     func updateUIView(_ uiView: WKWebView, context: Context) {
         let blankPage = "<!DOCTYPE html><html><body></body></html>"
         uiView.loadHTMLString(blankPage, baseURL: nil)
+        uiView.evaluateJavaScript(javascriptString) { result, error in
+            print(result ?? "No result value")
+            print(error ?? "No error")
+            
+            if let completionHandler = completionHandler {
+                completionHandler(result, error)
+            }
+        }
     }
 }
 
